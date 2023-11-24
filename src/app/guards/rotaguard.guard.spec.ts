bbc674b40a82c709eb19f13e21b7c390ee007b69
Arthur Henrique Vieira Busanello <tuti.busanello@hotmail.com>
2023-11-24 20:38:57 -0300

    testes 50% a

--- src/app/guards/rotaguard.guard.spec.ts
+++ src/app/guards/rotaguard.guard.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 
 import { rotaguardGuard } from './rotaguard.guard';
 
 describe('rotaguardGuard', () => {
+  let guard = TestBed.inject(rotaguardGuard);
+  let router: Router;
   const executeGuard: CanActivateFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => rotaguardGuard(...guardParameters));
 
@@ -18,4 +20,18 @@ describe('rotaguardGuard', () => {
   it('should be created', () => {
     expect(executeGuard).not.toBe(null);
   })
+  // it('should redirect to login if not authenticated', fakeAsync(() => {
+  //   localStorage.removeItem('authToken'); 
+  //   const route = {} as ActivatedRouteSnapshot;
+  //   const state = {} as RouterStateSnapshot;
+
+  //   // canActivate might be async, so wrap in fakeAsync
+  //   const result = guard.canActivate(route, state);
+
+  //   tick(); // Simulate passage of time for async operations
+
+  //   expect(result).toBeFalse();
+  //   expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  // }));
+
 });
